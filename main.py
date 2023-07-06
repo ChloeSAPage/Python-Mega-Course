676ab8f612e0d70b62b6a15861ef82533526b6e5
ChloeSPage <chloe.s.a.page@Gmail.com>
2023-07-06 17:55:31 +0100

    Added a place to display created apps

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import streamlit as st
+import pandas
 
-st.set_page_config(layout="wide")
+st.set_page_config(page_title="Hi, I'm Chloe!", layout="centered")
 
 column1, column2 = st.columns(2)
 
 with column1:
-    st.image("images\graduation photo - Copy.jpg", width = 400)
+    st.image("images\graduation photo - Copy.jpg", use_column_width=True)
+    st.write("Below are some of my projects! Feel free to view the source code.")
 
 with column2:
     st.title("Chloe Page")
@@ -18,3 +20,22 @@ I'm currently working on building my portfolio, so stay tuned for updates!
 If you have any questions or exciting opportunities in the biotech world, feel free to reach out. Let's connect and explore the intersection of biotech and coding together!
 """
     st.info(content)
+
+col3, empty_col, col4 =st.columns([1.5, 0.5, 1.5])
+
+data = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in data[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+
+with col4:
+    for index, row in data[10:].iterrows():
+            st.header(row["title"])
+            st.write(row["description"])
+            st.image("images/" + row["image"])
+           
+
