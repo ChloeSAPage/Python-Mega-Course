7fbb4df800908413f0dfd8db113a62e965b5c08f
ChloeSPage <chloe.s.a.page@Gmail.com>
2023-07-07 18:21:07 +0100

    Fixed error

--- send_email.py
+++ send_email.py
@@ -6,10 +6,10 @@ def send_email(user_message):
     port = 465
 
     username = "chloe.s.a.page@gmail.com"
-    password = os.getenv("")
+    password = os.getenv("GMAIL_PASSWORD")
 
     receiver = "chloe.s.a.page@gmail.com"
-    context = ssl.create_default_context("GMAIL_PASSWORD")
+    context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context= context) as server:
         server.login(username, password)
