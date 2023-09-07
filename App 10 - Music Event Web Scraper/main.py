d98f3f3272c68973bd3ea93155fb317cceed5799
ChloeSAPage <chloe.s.a.page@Gmail.com>
2023-09-07 13:45:08 +0100

    added working send email function

--- App 10 - Music Event Web Scraper/main.py
+++ App 10 - Music Event Web Scraper/main.py
@@ -1,5 +1,7 @@
 import requests
 import selectorlib
+import smtplib, ssl
+import os
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
@@ -19,7 +21,19 @@ def extract(source):
     return value
 
 
-def send_email():
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "chloe.s.a.page@gmail.com"
+    password = os.getenv("GMAIL_PASSWORD")
+
+    receiver = "chloe.s.a.page@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context= context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
     print("Email was sent!")
 
 
@@ -41,4 +55,4 @@ if __name__ == "__main__":
     if extracted != "No upcoming tours":
         if extracted not in content:
             store(extracted)
-            send_email()
+            send_email(message="New event found!")
