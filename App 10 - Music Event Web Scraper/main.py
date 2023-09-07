cff128fb1f9c3fe9e9966e11ff7a4bd9b098682c
ChloeSAPage <chloe.s.a.page@Gmail.com>
2023-09-07 13:34:57 +0100

    stored next tours and added multiple functions

--- App 10 - Music Event Web Scraper/main.py
+++ App 10 - Music Event Web Scraper/main.py
@@ -13,8 +13,32 @@ def scrape(url):
     return source
 
 
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
 
+
+def send_email():
+    print("Email was sent!")
+
+
+def store(extracted):
+    with open("data.txt", "a") as file:
+        file.write(extracted + "\n")
+
+def read(extracted):
+    with open("data.txt", "r") as file:
+        return file.read()
 
 
 if __name__ == "__main__":
-    print(scrape(URL))
+    scraped = scrape(URL)
+    extracted = extract(scraped)
+    print(extracted)
+    
+    content = read(extracted)
+    if extracted != "No upcoming tours":
+        if extracted not in content:
+            store(extracted)
+            send_email()
