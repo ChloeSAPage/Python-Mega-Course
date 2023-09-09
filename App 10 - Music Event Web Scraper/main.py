fb666197542e804c6b4c26f82e539dc680ee10cc
ChloeSAPage <chloe.s.a.page@Gmail.com>
2023-09-09 21:31:06 +0100

    Added Classes

--- App 10 - Music Event Web Scraper/main.py
+++ App 10 - Music Event Web Scraper/main.py
@@ -9,63 +9,74 @@ URL = "https://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-connection = sqlite3.connect("data.db")
-
-def scrape(url):
-    """Scrape the page source from URL"""
-    response = requests.get(url, headers=HEADERS)
-    source = response.text
-    return source
 
 
-def extract(source):
-    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
-    value = extractor.extract(source)["tours"]
-    return value
+class Event:
+    def scrape(self, url):
+        """Scrape the page source from URL"""
+        response = requests.get(url, headers=HEADERS)
+        source = response.text
+        return source
+
+    def extract(self, source):
+        extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+        value = extractor.extract(source)["tours"]
+        return value
 
 
-def send_email(message):
-    host = "smtp.gmail.com"
-    port = 465
+class Email:
+    def send(self, message):
+        host = "smtp.gmail.com"
+        port = 465
 
-    username = "chloe.s.a.page@gmail.com"
-    password = os.getenv("GMAIL_PASSWORD")
+        username = "chloe.s.a.page@gmail.com"
+        password = os.getenv("GMAIL_PASSWORD")
 
-    receiver = "chloe.s.a.page@gmail.com"
-    context = ssl.create_default_context()
+        receiver = "chloe.s.a.page@gmail.com"
+        context = ssl.create_default_context()
 
-    with smtplib.SMTP_SSL(host, port, context= context) as server:
-        server.login(username, password)
-        server.sendmail(username, receiver, message)
-    print("Email was sent!")
+        with smtplib.SMTP_SSL(host, port, context= context) as server:
+            server.login(username, password)
+            server.sendmail(username, receiver, message)
+        print("Email was sent!")
 
 
-def store(extracted):
-    row = extracted.split(",")
-    row = [item.strip() for item in row]
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
-    connection.commit()
+class Database:
 
-def read(extracted):
-    row = extracted.split(",")
-    row = [item.strip() for item in row]
-    band, city, date = row
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?",(band, city, date))
-    rows = cursor.fetchall()
-    print(rows)
-    return rows
+    def __init__(self, database_path):
+        self.connection = sqlite3.connect(database_path)
+
+    connection = sqlite3.connect("data.db")
+    def store(self, extracted):
+        row = extracted.split(",")
+        row = [item.strip() for item in row]
+        cursor = self.connection.cursor()
+        cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+        self.connection.commit()
+
+    def read(self, extracted):
+        row = extracted.split(",")
+        row = [item.strip() for item in row]
+        band, city, date = row
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?",(band, city, date))
+        rows = cursor.fetchall()
+        print(rows)
+        return rows
+
 
 if __name__ == "__main__":
     while True:
-        scraped = scrape(URL)
-        extracted = extract(scraped)
+        event = Event()
+        scraped = event.scrape(URL)
+        extracted = event.extract(scraped)
         print(extracted)
         
         if extracted != "No upcoming tours":
-            row = read(extracted)
+            database = Database(database_path="data.db")
+            row = database.read(extracted)
             if not row:
-                store(extracted)
-                send_email(message="New event found!")
+                database.store(extracted)
+                email = Email()
+                email.send(message="New event found!")
         time.sleep(2)
