3ac6fe4808a62574a883e0370fb478bf9adcb838
ChloeSAPage <chloe.s.a.page@Gmail.com>
2023-09-20 17:36:58 +0100

    added CreditCard class

--- App 11 - Hotel Booking App - OOP Style/main.py
+++ App 11 - Hotel Booking App - OOP Style/main.py
@@ -1,7 +1,7 @@
 import pandas
 
 df = pandas.read_csv("hotels.csv", dtype={"id": str})
-
+df_cards = pandas.read_csv("cards.csv", dtype=str).to_dict(orient="records")
 
 class Hotel:
     def __init__(self, hotel_id):
@@ -35,14 +35,32 @@ Hotel name: {self.hotel.name}
 """
         return content
 
+class CreditCard():
+    def __init__(self, number):
+        self.number = number
+
+    def validate(self, expiration, holder, cvc):
+        card_data = {"number":self.number, "expiration": expiration, 
+                      "holder": holder, "cvc": cvc}
+        if card_data in df_cards:
+            return True
+        else:
+            return False
+
+
+# Main
 print(df)
 hotel_ID = input("Enter id of the hotel: ")
 hotel = Hotel(hotel_ID)
 
 if hotel.available():
-    hotel.book()
-    name = input("Enter your name: ")
-    reservationticket = ReservationTicket(customer_name=name, hotel_object=hotel)
-    print(reservationticket.generate())
+    credit_card = CreditCard(number="1234")
+    if credit_card.validate(expiration="12/26", holder="JOHN SMITH", cvc="123"):
+        hotel.book()
+        name = input("Enter your name: ")
+        reservationticket = ReservationTicket(customer_name=name, hotel_object=hotel)
+        print(reservationticket.generate())
+    else:
+        print("There was a problem with your payment")
 else:
     print("Hotel is not available")
