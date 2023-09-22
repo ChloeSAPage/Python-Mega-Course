829eeef1bca19b703cd319add487e848cedeb866
ChloeSAPage <chloe.s.a.page@Gmail.com>
2023-09-22 18:00:23 +0100

    added submit

--- App 16 - Job Application App - Flask/app.py
+++ App 16 - Job Application App - Flask/app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    print(request.method)
+    if request.method =="POST":
+        first_name = request.form["first_name"]
+        print(first_name)
+    
     return render_template("index.html")
 
 
