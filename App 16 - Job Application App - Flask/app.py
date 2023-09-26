bc7fe47c4dc29e52dba5f22fa0513f9a4a532871
ChloeSAPage <chloe.s.a.page@Gmail.com>
2023-09-26 16:25:57 +0100

    SQLite to store user data

--- App 16 - Job Application App - Flask/app.py
+++ App 16 - Job Application App - Flask/app.py
@@ -1,14 +1,32 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+
+app.config.from_pyfile("config.py")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+db = SQLAlchemy(app)
+
+
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    date = db.Column(db.Date)
+    occupation = db.Column(db.String(80))
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    print(request.method)
     if request.method =="POST":
         first_name = request.form["first_name"]
-        print(first_name)
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        date = request.form["date"]
+        occupation = request.form["occupation"]
+        
     
     return render_template("index.html")
 
@@ -16,6 +34,7 @@ def index():
 
 
 
-
-
-app.run(debug=True, port=5001)
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True, port=5001)
