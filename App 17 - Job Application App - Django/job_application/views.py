8b0caa0beb5276f577b98496edf2ebeb1fc64356
ChloeSAPage <chloe.s.a.page@Gmail.com>
2023-09-27 21:10:49 +0100

    form

--- App 17 - Job Application App - Django/job_application/views.py
+++ App 17 - Job Application App - Django/job_application/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from .forms import ApplicationForm
 
 def index(request):
+    if request.method == "POST":
+        form = ApplicationForm(request.post)
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            date = form.cleaned_data["date"]
+            occupation = form.cleaned_data["occupation"]
     return render(request, "index.html")
