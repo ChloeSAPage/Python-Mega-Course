214c7960dc0f0dcae569ae88cd1ddf95eac8c8e1
ChloeSAPage <chloe.s.a.page@Gmail.com>
2023-10-11 21:41:18 +0100

    added admin

--- App 17 - Job Application App - Django/job_application/admin.py
+++ App 17 - Job Application App - Django/job_application/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Form
 
 # Register your models here.
+
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email")
+    search_fields = ("first_name", "last_name", "email")
+    list_filter = ("date", "occupation")
+    ordering = ("-first_name", )
+    readonly_fields = ("occupation", )
+
+admin.site.register(Form, FormAdmin)
